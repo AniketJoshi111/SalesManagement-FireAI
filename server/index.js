fadecdd2b6894e760998552aa267b7d751f175df
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-11-15 23:34:01 +0530

    final

--- server/index.js
+++ server/index.js
@@ -3,15 +3,28 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const connectDB = require('./db');
 require('dotenv').config();
+const router = require('./routes/sales')
 
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-connectDB();
 
+// Make sure you link the sales.js to a specific route
+app.use('/', router);
 
+const startServer = async() => {
+    try{
+        await connectDB()
+        app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+    }
+    catch(error){
+        console.error("Error during server startup")
+        console.log(error.message);
+    }
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+startServer()
