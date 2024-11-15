f34d987507fd037dfe1121158b964100bfe1bd09
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-11-15 21:52:10 +0530

    modelling and routes done

--- server/index.js
+++ server/index.js
@@ -8,16 +8,13 @@ require('dotenv').config();
 // const salesRoutes = require('./routes/sales');
 
 const app = express();
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
 
 app.use(cors());
 app.use(express.json());
 
-// mongoose.connect(process.env.MONGO_URI)
-//   .then(() => console.log('MongoDB connected'))
-//   .catch(err => console.error(err));
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error(err));
 
 // app.use('/users', userRoutes);
 // app.use('/sales', salesRoutes);
