140514a40bd516f41d6726ca2f4b2ccfe37e878c
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-11-15 21:53:22 +0530

    modelling and routes done

--- server/index.js
+++ server/index.js
@@ -4,8 +4,6 @@ const cors = require('cors');
 
 require('dotenv').config();
 
-// const userRoutes = require('./routes/users');
-// const salesRoutes = require('./routes/sales');
 
 const app = express();
 
@@ -16,8 +14,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
-// app.use('/users', userRoutes);
-// app.use('/sales', salesRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
