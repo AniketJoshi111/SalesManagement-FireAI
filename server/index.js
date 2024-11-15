6ad6cdffe9aa8171cc6e627598bb69f50ea806c6
AniketJoshi111 <aniketjoshi2606@gmail.com>
2024-11-15 21:59:40 +0530

    db file done

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-
+const connectDB = require('./db');
 require('dotenv').config();
 
 
@@ -9,10 +9,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error(err));
+connectDB();
 
 
 
